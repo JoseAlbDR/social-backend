b46e416a4b0b25ce8ecb013a68668ed24d1d742c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-11 15:49:18 +0200

    feag: add validateConfig method to config class

--- src/config.ts
+++ src/config.ts
@@ -12,6 +12,7 @@ class Config {
 
   private readonly DEFAULT_DATABASE_URL: string =
     "mongodb://127.0.0.1:27017/socialapp-backend";
+
   constructor() {
     this.DATABSE_URL = process.env.DATABASE_URL || this.DEFAULT_DATABASE_URL;
     this.JWT_TOKEN = process.env.JWT_TOKEN || "1234";
@@ -19,6 +20,14 @@ class Config {
     this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || "";
     this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || "";
     this.CLIENT_URL = process.env.CLIENT_URL || "";
+  }
+
+  public validateConfig(): void {
+    for (const [key, value] of Object.entries(this)) {
+      if (value === undefined) {
+        throw new Error(`${key} is not defined`);
+      }
+    }
   }
 }
 export const config: Config = new Config();
