cf8cfa8944c5e20c396396cf11c39500a03bb510
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-11 15:31:53 +0200

    feat: connect to Database in app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Express } from "express";
 import { SocialServer } from "./setupServer";
+import dbConnection from "./setupDatabase";
 
 class Application {
   public initialize(): void {
+    dbConnection();
     const app: Express = express();
     const server: SocialServer = new SocialServer(app);
     server.start();
