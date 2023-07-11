bc97503ec6673e2cb3be734cbc878d8ec343a486
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-11 15:49:54 +0200

    feat: validate config inside Application class

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express, { Express } from "express";
 import { SocialServer } from "./setupServer";
 import dbConnection from "./setupDatabase";
+import { config } from "./config";
 
 class Application {
   public initialize(): void {
+    this.loadConfig();
     dbConnection();
     const app: Express = express();
     const server: SocialServer = new SocialServer(app);
     server.start();
+  }
+
+  private loadConfig(): void {
+    config.validateConfig();
   }
 }
 
